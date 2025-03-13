d6e6157b0f1a0c19801e18530c65a103ba722feb
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2025-03-13 02:09:22 -0400

    Add Header Component

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,8 +2,20 @@ import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "./ui/button";
-import { LayoutDashboard } from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import {
+  ChevronDown,
+  FileText,
+  GraduationCap,
+  LayoutDashboard,
+  PenBox,
+  StarsIcon,
+} from "lucide-react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "./ui/dropdown-menu";
 
 const Header = () => {
   return (
@@ -21,35 +33,66 @@ const Header = () => {
             className="h-14 py-1 w-auto object-contain"
           />
         </Link>
-        <div>
+        <div className="flex items-center space-x-2 md:space-x-4">
           <SignedIn>
             <Link href={"/dashboard"}>
-              <Button>
+              <Button variant="outline">
                 <LayoutDashboard className="w-4 h-4" />
                 <span className="hidden md:block">Industry Insights</span>
               </Button>
             </Link>
+            <DropdownMenu>
+                <DropdownMenuTrigger>
+                <Button>
+                    <StarsIcon className="w-4 h-4" />
+                    <span className="hidden md:block">Growth Tools</span>
+                    <ChevronDown className="w-4 h-4" />
+                </Button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                <DropdownMenuItem>
+                    <Link href={"/resume"} className="flex items-center gap-2">
+                    <FileText className="w-4 h-4" />
+                    <span>Build Resume</span>
+                    </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Link
+                    href={"/ai-cover-letter"}
+                    className="flex items-center gap-2"
+                    >
+                    <PenBox className="w-4 h-4" />
+                    <span>Cover Letter</span>
+                    </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                    <Link href={"/interview"} className="flex items-center gap-2">
+                    <GraduationCap className="w-4 h-4" />
+                    <span>Interview Prep</span>
+                    </Link>
+                </DropdownMenuItem>
+                </DropdownMenuContent>
+            </DropdownMenu>
           </SignedIn>
-          <DropdownMenu>
-            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuLabel>My Account</DropdownMenuLabel>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>Profile</DropdownMenuItem>
-              <DropdownMenuItem>Billing</DropdownMenuItem>
-              <DropdownMenuItem>Team</DropdownMenuItem>
-              <DropdownMenuItem>Subscription</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <SignedOut>
+            <SignInButton>
+                <Button variant="outline">Sign In</Button>
+            </SignInButton>
+          </SignedOut>
+          <SignedIn>
+            <UserButton 
+            appearance={{
+                elements:{
+                    avatarBox: 'w-10 h-10',
+                    userButtonPopoverCard: "shadow-xl",
+                    userPreviewMainIdentifier: "font-semibold"
+                }
+            }}
+            afterSignOutUrl="/"
+            />
+          </SignedIn>
         </div>
       </nav>
-      <SignedOut>
-        <SignInButton />
-        {/* <SignUpButton /> */}
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
     </header>
   );
 };
