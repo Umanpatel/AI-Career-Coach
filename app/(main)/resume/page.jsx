8467c0649d726f8e18db45793198a594ed517f30
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2025-04-05 17:07:50 -0400

    Add Server Action: improveWithAI

---
+++ app/(main)/resume/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ResumePage = async () => {
+    const resume = await getResume();
+    return (
+        <div>
+            <ResumeBuilder/>
+        </div>
+    )
+}
+
+export default ResumePage
