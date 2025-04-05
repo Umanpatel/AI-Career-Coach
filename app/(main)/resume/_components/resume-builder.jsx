90a9e59ee33aabffa77f927329fd1c5d49e7ca54
Umang Maheshkumar Patel <umpatel19041999@gmail.com>
2025-04-05 17:37:51 -0400

    Add contactSchema, entrySchema and resumeSchema (combine contactSchema and entrySchema) in "app->lib->schema.js"

---
+++ app/(main)/resume/_components/resume-builder.jsx
@@ -0,0 +1,68 @@
+"use client";
+import { Button } from "@/components/ui/button";
+import { TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Tabs } from "@radix-ui/react-tabs";
+import { Download, Loader2, Save } from "lucide-react";
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+
+const ResumeBuilder = ({ initialContent }) => {
+    const [activeTab, setActiveTab] = useState("edit");
+
+    useForm
+
+  return (
+    <div data-color-mode="light" className="space-y-4">
+      <div className="flex flex-col md:flex-row justify-between items-center gap-2">
+        <h1 className="font-bold gradient-title text-5xl md:text-6xl">
+          Resume Builder
+        </h1>
+        <div className="space-x-2">
+          <Button
+            variant="destructive"
+            onClick={handleSubmit(onSubmit)}
+            disabled={isSaving}
+          >
+            {isSaving ? (
+              <>
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                Saving...
+              </>
+            ) : (
+              <>
+                <Save className="h-4 w-4" />
+                Save
+              </>
+            )}
+          </Button>
+          <Button onClick={generatePDF} disabled={isGenerating}>
+            {isGenerating ? (
+              <>
+                <Loader2 className="h-4 w-4 animate-spin" />
+                Generating PDF...
+              </>
+            ) : (
+              <>
+                <Download className="h-4 w-4" />
+                Download PDF
+              </>
+            )}
+          </Button>
+        </div>
+      </div>
+
+      <Tabs value={activeTab} onValueChange={setActiveTab}>
+        <TabsList>
+          <TabsTrigger value="edit">Form</TabsTrigger>
+          <TabsTrigger value="preview">Markdown</TabsTrigger>
+        </TabsList>
+        <TabsContent value="edit">
+          Make changes to your account here.
+        </TabsContent>
+        <TabsContent value="preview">Change your password here.</TabsContent>
+      </Tabs>
+    </div>
+  );
+};
+
+export default ResumeBuilder;
--- app/(main)/resume/page.jsx
+++ app/(main)/resume/page.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import ResumeBuilder from './_components/resume-builder';
 
 const ResumePage = async () => {
     const resume = await getResume();
     return (
-        <div>
-            <ResumeBuilder/>
+        <div className="container mx-auto py-6">
+            <ResumeBuilder initialContent={resume?.content}/>
         </div>
     )
 }
